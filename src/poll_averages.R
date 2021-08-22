4efaff6d1fd1b7a4945c7901a68a12cc38855e8b
Mac Tan <thisismactan@users.noreply.github.com>
2021-08-22 18:17:42 -0400

    updated forecast 22 Aug 2021

--- src/poll_averages.R
+++ src/poll_averages.R
@@ -6,7 +6,8 @@ natl_polls <- read_csv("data/polls_2021_natl.csv") %>%
   dplyr::select(-rating, -lead) %>%
   melt(measure.vars = c("Liberal", "Conservative", "NDP", "Green", "Bloc", "People's"), variable.name = "party", value.name = "pct") %>%
   mutate(pct = pct / 100,
-         loess_weight = n^0.25 / ifelse(pollster %in% c("Nanos Research", "Forum Research", "EKOS"), 4, 1),
+         loess_weight = n^0.25 / (ifelse(pollster %in% c("Forum Research", "EKOS"), 4, 1) *
+                                    ifelse(pollster %in% c("Mainstreet Research", "Nanos Research"), 3, 1)),
          age = as.numeric(today() - median_date)) %>%
   dplyr::select(pollster, median_date, age, n, loess_weight, party, pct) %>%
   as_tibble()
